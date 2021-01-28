0584f3f0cfa4b6edc1ee0f122eb8736d64f1119e
Luke Chapman <Luke.Chapman@ccr.net>
2021-01-27 22:21:10 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('#company').innerText = "Joe's Bed and Breakfast"
+
+document.querySelector('#slogan').innerHTML = 'The best lodging and eatery <br>west of the Mississippi!'
+
+let userName = prompt('What is your name?');
+
+let greeting = `Hello ${userName}, we are glad you're here!`
+
+document.querySelector('#greeting').innerText = greetingLuke;
